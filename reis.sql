cf5001daedfa67b1b2caa923d412cf88e4f54ac3
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2024-09-12 14:40:22 +0300

    Add files via upload

---
+++ reis.sql
@@ -0,0 +1,40 @@
+create table reis (
+	reis_id INT,
+	transport VARCHAR(50),
+	lahtekoht VARCHAR(50),
+	saabumsiskoht VARCHAR(50),
+	hind VARCHAR(50),
+	kuupaev DATE,
+	väljumisaeg VARCHAR(50),
+	lennufirma VARCHAR(50)
+);
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (1, 'WUAGNAFG7BN612344', 'Langtad', 'Huangtang', '€357,44', '12/15/2023', '1:04 PM', 'MICTW');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (2, '1GKS1EEF0BR411210', 'Ovidiopol’', 'Toledo', '€212,46', '12/23/2023', '9:12 PM', 'ONB');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (3, 'JH4KB16627C302210', 'San Rafael Abajo', 'Jagiełła', '€68,16', '10/18/2023', '7:00 PM', 'KTOVW');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (4, '2C3CDXHG7EH960872', 'Tairua', 'Belo Jardim', '€351,94', '12/29/2023', '9:57 AM', 'AMC');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (5, 'WAUBGAFC5CN913658', 'Abuxarda', 'Guimarei', '€316,92', '7/11/2024', '12:06 AM', 'CIB');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (6, '1FMNE1BW2AD480575', 'Basiao', 'Klatovy', '€250,74', '7/26/2024', '4:38 AM', 'CMFN');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (7, 'W04GY5GV2B1874475', 'Trujillo', 'Santiago del Torno', '€340,76', '1/15/2024', '3:13 PM', 'TMUSP');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (8, 'WAUNF98P67A815685', 'Teculután', 'Belén Gualcho', '€301,84', '4/15/2024', '2:58 AM', 'DFBG');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (9, '1C3BC7EG4BN030790', '‘Afak', 'Leganes', '€259,20', '10/5/2023', '7:38 AM', 'SMTC');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (10, 'WAULD54B43N016975', 'Ibiporã', 'Naghalin', '€336,79', '10/24/2023', '6:14 PM', 'BMO');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (11, 'WAUEFBFL6BN469033', 'Atouguia', 'Watuagung', '€287,31', '10/27/2023', '11:40 PM', 'ACH');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (12, '1G6KD57989U012033', 'Castro Barros', 'Betulia', '€106,99', '1/28/2024', '10:54 PM', 'MPAC');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (13, '5TDBKRFH9ES341678', 'Longshan', 'San Fernando', '€240,67', '6/1/2024', '6:02 AM', 'ADM');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (14, 'WBAVB73518K006452', 'Zeya', 'Ceres', '€264,88', '9/7/2024', '5:49 PM', 'RRTS');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (15, '5TFCW5F14CX360702', 'Nasielsk', 'Munjungan', '€296,66', '12/23/2023', '4:08 AM', 'FNGN');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (16, '4A31K2DF1BE803847', 'Gaillac', 'Kosti', '€191,16', '11/26/2023', '7:09 PM', 'DL');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (17, 'KNADM5A32E6786171', 'Vrané nad Vltavou', 'Duanshen', '€388,34', '1/17/2024', '9:52 PM', 'BVXV');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (18, 'JN8AF5MR8FT831458', 'Ocumare del Tuy', 'Houston', '€217,88', '2/28/2024', '11:58 PM', 'COE');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (19, '1HGCP2E3XCA390922', 'Changping', 'Przytyk', '€158,06', '2/21/2024', '10:24 PM', 'LPNT');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (20, '19UUA65625A454803', 'Velikiye Luki', 'Nong Muang Khai', '€284,88', '4/3/2024', '5:16 AM', 'SYX');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (21, 'KNADH4A36A6662087', 'Dewetsdorp', 'Baijiang', '€69,38', '11/14/2023', '4:12 AM', 'PSEC');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (22, '5N1CR2MM0EC734053', 'Sidareja', 'Ulimonong', '€342,00', '4/14/2024', '4:35 PM', 'BSPM');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (23, 'WBAUP9C55AV716645', 'Chřibská', 'Alfena', '€184,20', '9/22/2023', '5:18 PM', 'HYGS');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (24, 'WBA5M0C52ED357727', 'Banjarjo', 'Jatinagara Kulon', '€395,41', '8/10/2024', '11:29 PM', 'TRMK');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (25, '3GYFNBE35ES584979', 'Rio do Sul', 'Sangke', '€56,60', '8/4/2024', '10:13 PM', 'RPAI^A');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (26, '3D73Y3HL7BG493962', 'Taganak', 'Sun Kunda', '€281,81', '1/20/2024', '6:41 AM', 'TTOO');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (27, 'JH4KC1F51EC304077', 'Danyang', 'Corralito', '€337,85', '3/8/2024', '6:02 AM', 'OBCI');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (28, '3VW507AT7FM584911', 'Paingar', 'Ansan-si', '€94,22', '2/14/2024', '10:37 PM', 'HSGX');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (29, '3FA6P0PU3ER629439', 'Baitoa', 'Al Marj', '€80,12', '12/2/2023', '12:44 PM', 'AOS');
+insert into reis (reis_id, transport, lahtekoht, saabumsiskoht, hind, kuupaev, väljumisaeg, lennufirma) values (30, '1G4GE5G36CF570448', 'Azovo', 'Ryūgasaki', '€162,04', '8/23/2024', '7:00 PM', 'CDL');
